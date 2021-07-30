3574f5c9418e52e12d7eba6d655d2d55b35ea318
mudde <olaf.mudde@xs4all.nl>
2021-07-30 13:27:02 +0200

    Update example

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,46 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
-const glob = require("glob")
-
 const isProduction = process.env.NODE_ENV == "production";
-
 const config = {
   entry: '/src/index.ts',
   devtool: 'source-map',
   output: {
-     library: {
+    library: {
       name: 'MuddeCore',
       type: 'umd'
     },
     filename: 'core.js',
     path: path.resolve(__dirname, "dist")
-    
   },
-  plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
         loader: "ts-loader",
-        exclude: ["/node_modules/", "/test/"],
+        exclude: [
+          path.resolve(__dirname, "node_modules"),
+          path.resolve(__dirname, "test"),
+        ],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
+  },
+  stats: {
+    assets: true,
+    colors: false,
+    errors: true,
+    errorDetails: true,
+    hash: true,
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      MuddeCore: path.resolve(__dirname, 'src/')
+    },
   }
 };
 
