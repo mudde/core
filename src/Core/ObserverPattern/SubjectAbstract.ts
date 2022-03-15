76946d90d2a5227f34638f16b82498af20c7213a
mudde <olaf.mudde@xs4all.nl>
2022-03-15 13:54:03 +0100

    Subject supports anonymouse functions

--- src/Core/ObserverPattern/SubjectAbstract.ts
+++ src/Core/ObserverPattern/SubjectAbstract.ts
@@ -14,16 +14,16 @@ export abstract class SubjectAbstract implements SubjectInterface {
    private _observers: any[] = []
    private _pause: any[] = []
 
-   attach(eventNumber: number, observer: ObserverInterface): void {
+   attach(eventNumber: number, observer: ObserverInterface | CallableFunction): void {
       this._observers[eventNumber] = this._observers[eventNumber] ?? []
       this._observers[eventNumber].push(observer)
    }
 
-   detach(observer: ObserverInterface): void {
+   detach(observer: ObserverInterface | CallableFunction): void {
       this._observers.forEach(
          (observerList) => {
             observerList.flatten(
-               (item: ObserverInterface) => { return item !== observer })
+               (item: ObserverInterface | CallableFunction) => { return item !== observer })
          }
       )
    }
@@ -31,19 +31,30 @@ export abstract class SubjectAbstract implements SubjectInterface {
    notify(source: any, eventNumber: number = null): void {
       var event = source instanceof Event ? source : new Event(source, eventNumber)
       let pause = this._pause
-      
+
       if (this._observers[eventNumber]) {
          this._observers[eventNumber].forEach(element => {
-            pause.indexOf(element) != -1 || element.update(event)
+            pause.indexOf(element) != -1 || typeof element === 'function'
+               ? element(event)
+               : element.update(event)
          })
       }
    }
 
-   pauseAttach(observer: ObserverInterface): void {
+   pauseAttach(observer: ObserverInterface | CallableFunction): void {
       let pause = this._pause
 
       if (pause.indexOf(observer) == -1) {
          this._pause.push(observer)
+      }
+   }
+
+   pauseDetach(observer: ObserverInterface | CallableFunction): void {
+      let pause = this._pause
+      let indexOf = pause.indexOf(observer)
+
+      if (indexOf !== -1) {
+         this._pause = [...pause.slice(0, indexOf), ...pause.slice(indexOf + 1)]
       }
    }
 
