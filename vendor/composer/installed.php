b37f5f12852467e52afacf57f07360093ce1a76d
mudde <olaf.mudde@xs4all.nl>
2022-04-10 00:01:51 +0200

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ead5f26ece523885116e363a3ef4e62c33839d86',
+        'name' => 'mudde/core',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mudde/core' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ead5f26ece523885116e363a3ef4e62c33839d86',
+            'dev_requirement' => false,
+        ),
+    ),
+);
